af903bcac8d1a8bc7bd9ba7abdafd8f649f63463
20171105124 <145477517@qq.com>
2018-06-27 10:43:44 +0800

    Initial Commit

---
+++ homework of software engineering/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  homework of software engineering
+//
+//  Created by nico_d on 2018/6/27.
+//  Copyright © 2018年 nico_d. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
